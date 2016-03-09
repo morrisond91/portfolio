ca667000ddcb49cdde32d9625a20d963bb3d7b24
Ruben Rodrigues <neburdesign@gmail.com>
2016-03-09 23:38:12 +0000

    pt first commit

---
+++ js/map.js
@@ -0,0 +1,56 @@
+jQuery(function($){
+
+	var longitude = 39.482447;
+	var latitude = -8.332477;
+	var canvas = "map";
+
+	
+	function randing_map(canvas, lan, lat){			
+			var myLatlng = new google.maps.LatLng(lan,lat);
+			var myOptions = {
+						zoom: 8,
+						center: myLatlng,
+						mapTypeId: google.maps.MapTypeId.ROADMAP,
+						maxZoom   : 20,
+    					disableDefaultUI: true
+					}			
+			var map = new google.maps.Map( document.getElementById(canvas), myOptions );
+			var marker = new google.maps.Marker({
+			    position : myLatlng,
+			    map      : map,
+			    icon     : "img/point.png"
+			});
+			var styles = [
+			  {
+			    featureType: "all",
+			    stylers: [
+			      { saturation: -80 }
+			    ]
+			  },{
+			    featureType: "road.arterial",
+			    elementType: "geometry",
+			    stylers: [
+			      { hue: "#00ffee" },
+			      { saturation: 50 }
+			    ]
+			  },{
+			    featureType: "poi.business",
+			    elementType: "labels",
+			    stylers: [
+			      { visibility: "off" }
+			    ]
+			  }
+			];	
+			var infowindow = new google.maps.InfoWindow({
+				content:"<div class='map_adresse'><div class='map_address'><span class='address'>Address : </span>Rua D. Carlos I Bloco C 2º Dir. Constância, Portugal</div> <div class='map_email'><span class='email'>Email : </span><a href='mailto:support@inebur.com?subject=New%20Project%20Enquiries%20or%20Saying%20Hello!' class='mail-to'>info<span class='color-text'>@</span>inebur.com</a></div> <div class='map_tel'><span class='tel'>Phone : </span>(+351) 912827471</div></div></div>"
+			});	
+			
+			map.setOptions({styles: styles});
+
+			google.maps.event.addListener(marker, 'click', function() {
+			  infowindow.open(map,marker);
+			});
+	}
+	randing_map(canvas, longitude, latitude);
+
+});
